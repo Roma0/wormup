7b8fd0ffa50da56586d7f62e19ffb782a3bb70ba
Roma0 <yd13_0@163.com>
2020-02-11 11:29:24 -0500

    add spring @Repository annotation

--- src/main/java/com/ascending/repository/DepartmentDaoImpl.java
+++ src/main/java/com/ascending/repository/DepartmentDaoImpl.java
@@ -8,9 +8,11 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class DepartmentDaoImpl implements DepartmentDao {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
