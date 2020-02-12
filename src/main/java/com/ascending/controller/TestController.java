32d7b9b15bb60c2074355f236b0a56775fd45f03
Roma0 <yd13_0@163.com>
2020-02-12 17:05:07 -0500

    add @PathVarible and @RequestParam annotated methods

--- src/main/java/com/ascending/controller/TestController.java
+++ src/main/java/com/ascending/controller/TestController.java
@@ -3,6 +3,7 @@ package com.ascending.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,12 @@ import java.util.Random;
 @RequestMapping(value = {"/test"})
 public class TestController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/path/{pathValue1}/{pathValue2}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String getPath( @PathVariable(name = "pathValue1") String p1, @PathVariable(name = "pathValue2") String p2) {
+        logger.info(String.format(">>>>>>>>>> Path value: %s, %s", p1, p2));
+        return p1 + ", " + p2;
+    }
 
     //GET /test
     /*
@@ -43,7 +50,7 @@ public class TestController {
     public Map<String, Object> createObject(){
         Map<String, Object> m = new HashMap<>();
         Random r = new Random();
-        m.put("id", 1);
+        m.put("id", r.nextInt());
         m.put("name", "HR");
         m.put("capacity", 500);
         logger.debug("create an object with id:" + m.get("id"));
