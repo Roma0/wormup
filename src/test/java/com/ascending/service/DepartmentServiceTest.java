eed0efe453c67d7da0492d5afdabfc69e2ef373c
Roma0 <yd13_0@163.com>
2020-02-26 01:45:43 -0500

    update DepartmentService and its unit test, passing

--- src/test/java/com/ascending/service/DepartmentServiceTest.java
+++ src/test/java/com/ascending/service/DepartmentServiceTest.java
@@ -2,7 +2,7 @@ package com.ascending.service;
 
 import com.ascending.ApplicationBootstrap;
 import com.ascending.model.Department;
-import com.ascending.repository.DepartmentDao;
+import com.ascending.model.Employee;
 import com.ascending.repository.EmployeeDao;
 import org.junit.After;
 import org.junit.Assert;
@@ -16,35 +16,92 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApplicationBootstrap.class)
 public class DepartmentServiceTest {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    @Autowired
-    DepartmentService departmentService;
+    @Autowired DepartmentService departmentService;
+    @Autowired EmployeeService employeeService;
+
+    private String deptName = "test";
+    private String emName1 = "yd";
+    private String emName2 = "lu";
 
     @Before
     public void setUp() {
         logger.debug("SetUp before testing ...");
         Department d1 = new Department();
-        d1.setName("test");
+        d1.setName(deptName);
         assert (0 != departmentService.save(d1).getId());
+
+        Employee e1 = new Employee();
+        e1.setName(emName1);
+        e1.setDepartment(d1);
+        assert (0 != employeeService.save(e1, deptName).getId());
+
+        Employee e2 = new Employee();
+        e2.setName(emName2);
+        e2.setDepartment(d1);
+        assert (0 != employeeService.save(e2, deptName).getId());
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         logger.debug("TearDown after testing ...");
-        assert (departmentService.delete("test"));
+        assert (employeeService.deleteByName(emName1));
+        assert (employeeService.deleteByName(emName2));
+        assert (departmentService.delete(deptName));
     }
 
     @Test
-    public void getDepartmentsTest() {
+    public void update(){
+        logger.debug(String.format("Testing %s ...", this.getClass().getName()));
+
+        Department expectedDepartment = departmentService.getDepartmentByName(deptName);
+        expectedDepartment.setLocation("location Changed");
+
+        Department department = departmentService.update(expectedDepartment);
+
+        Assert.assertEquals(expectedDepartment, department);
+    }
+
+
+    @Test
+    public void getDepartments() {
         logger.debug(String.format("Testing %s ...", this.getClass().getName()));
         List<Department> departments = departmentService.getDepartments();
-        int expectedNumOfDept = 5;
 
-//        departments.forEach(dept -> System.out.println(dept));
+        int expectedNumOfDept = 5;
         Assert.assertEquals(expectedNumOfDept, departments.size());
+    }
+
+    @Test
+    public void getDepartmentsWithChildren() {
+        logger.debug(String.format("Testing %s ...", this.getClass().getName()));
+        List<Department> departments = departmentService.getDepartmentsWithChildren();
+
+        int expectedNumOfDept = 5;
+        Assert.assertEquals(expectedNumOfDept, departments.size());
+
+        int expectedNumOfEmployee = 6;
+//        departments.forEach(dept -> System.out.println(dept.getEmployees()));
+        int sum = departments.stream().map(department ->
+                department.getEmployees().size()).mapToInt(Integer::intValue).sum();
+        Assert.assertEquals(expectedNumOfEmployee, sum);
+    }
+
+    //   Question1: Wrong way to test a method with another parallel method?
+    //   Question2: Need to getObject from the database again for assertion?
+
+    @Test
+    public void getDepartmentByName() {
+        logger.debug(String.format("Testing %s ...", this.getClass().getName()));
+
+        Department department = departmentService.getDepartmentByName(deptName);
+
+        Assert.assertEquals(deptName, department.getName());
+        Assert.assertEquals(2, department.getEmployees().size());
     }
 }
