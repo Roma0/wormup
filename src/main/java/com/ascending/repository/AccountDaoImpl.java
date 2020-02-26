ab602fe3af844cbd6f3232d9f9981aced568f84b
Roma0 <yd13_0@163.com>
2020-02-26 01:49:08 -0500

    add @Repository annotation to AccountDapImpl

--- src/main/java/com/ascending/repository/AccountDaoImpl.java
+++ src/main/java/com/ascending/repository/AccountDaoImpl.java
@@ -10,10 +10,12 @@ import org.hibernate.query.Query;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Repository
 public class AccountDaoImpl implements AccountDao{
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     private EmployeeDao employeeDao = new EmployeeDaoImpl();
