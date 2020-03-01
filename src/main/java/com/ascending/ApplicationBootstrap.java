678fb46eca4d71464ec7dfccd09b6d71797155fb
Roma0 <yd13_0@163.com>
2020-03-01 16:04:19 -0500

    Add 'filer' scanning package annotation into ApplicationBootstrap

--- src/main/java/com/ascending/ApplicationBootstrap.java
+++ src/main/java/com/ascending/ApplicationBootstrap.java
@@ -2,11 +2,14 @@ package com.ascending;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.ascending"})
+@ServletComponentScan(basePackages = {"com.ascending.filter"})
 public class ApplicationBootstrap {
 
     public static void main(String[] args){
         SpringApplication.run(ApplicationBootstrap.class, args);
     }
+
 }
