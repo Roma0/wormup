ddf6e16161afadf540051330c8b65529a151b9ce
Roma0 <yd13_0@163.com>
2020-03-01 18:32:41 -0500

    Modified Create_user_role_table.sql

--- src/main/resources/db/migration/V1.4__Create_user_role_table.sql
+++ src/main/resources/db/migration/V1.4__Create_user_role_table.sql
@@ -1,20 +1,22 @@
-CREATE TABLE user(
+CREATE TABLE users (
     id              BIGSERIAL NOT NULL,
-    name            VARCHAR (30) NOT NULL UNIQUE,
-    password        VARCHAR (54),
-    first_name      VARCHAR (30),
-    last_name       VARCHAR (30,
-    email           VARCHAR (50) NOT NULL UNIQUE,
-    primary key(id)
+    name            VARCHAR(30) NOT NULL UNIQUE,
+    password        VARCHAR(54),
+    first_name      VARCHAR(30),
+    last_name       VARCHAR(30),
+    email           VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE role(
+ALTER TABLE users ADD CONSTRAINT user_pk PRIMARY KEY (id);
+
+CREATE TABLE role (
     id                  BIGSERIAL NOT NULL,
     name                VARCHAR (30) not null unique,
     allowed_resource    VARCHAR (200),
     allowed_read        boolean not null default false,
     allowed_create      boolean not null default false,
     allowed_update      boolean not null default false,
-    allowed_delete      boolean not null default false,
-    primary key(id)
+    allowed_delete      boolean not null default false
 );
+
+ALTER TABLE role ADD CONSTRAINT role_pk PRIMARY KEY (id);
